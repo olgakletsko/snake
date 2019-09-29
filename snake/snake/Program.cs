a70d0f0fa884c3a85f8c8322de0a537670cdf5f2
Olga Kletsko <olya.klecko@gmail.com>
2019-09-29 23:26:02 +0300

    incapsulation

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,18 +6,12 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            point p1 = new point ();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sin = (*);
-            p1. Draw();
+            point p1 = new point (1,3,'*');
+                        p1. Draw();
 
 
-            point p2 = new point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sin = (#);
-                p2. Draw();
+            point p2 = new point(4,5,'#');
+                           p2. Draw();
 
            
             Console.ReadLine();
