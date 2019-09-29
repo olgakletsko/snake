b130e24cc3eac75f86c637a64f7ef4b86682a731
Olga Kletsko <olya.klecko@gmail.com>
2019-09-29 23:13:32 +0300

    class

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,8 +6,23 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            point p1 = new point ();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sin = (*);
+            p1. Draw();
+
+
+            point p2 = new point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sin = (#);
+                p2. Draw();
+
+           
             Console.ReadLine();
+
         }
+      
     }
 }
