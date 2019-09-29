e41d3877764f26b0c78ae0fdeae5ddd5c7c732f8
Olga Kletsko <olya.klecko@gmail.com>
2019-09-29 22:53:24 +0300

    draw

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -6,8 +6,18 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            int x1=1;
+            int y1 = 3;
+            char sin1 = '*';
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sin1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sin2 = '#';
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(sin2);
+
         }
     }
 }
