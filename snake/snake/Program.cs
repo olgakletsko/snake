d22bb77ef59822eac75633781d5c838371c5b981
Olga Kletsko <olya.klecko@gmail.com>
2019-09-29 23:51:23 +0300

    les5

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -12,9 +12,22 @@ namespace snake
 
             point p2 = new point(4,5,'#');
                            p2. Draw();
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
-           
-            Console.ReadLine();
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+            foreach (int i in numList) ;
+            {
+                Console.ReadLine();
+            }
+            numList.RemoveAt(0);
+            List<point> plist = new List<point>;
+            plist.Add(p1);
+            plist.Add(p2);
 
         }
       
